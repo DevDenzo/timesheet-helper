f38285fafb7a29b44bd2a9167fb3c2ce77cf7f6f
DevDenzo <dyukselir@hotmail.co.uk>
2022-12-13 01:16:20 +0000

    apps generated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'timesheet-helper',
   webDir: 'www',
   bundledWebRuntime: false
