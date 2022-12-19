f8cefa571651f91a7b433021c8bf9b2b719891bd
DevDenzo <dyukselir@hotmail.co.uk>
2022-12-19 15:03:17 +0000

    Loading Screen + Config Changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.uk.ey.timesheet-helper.app',
+  appId: 'com.uk.ey.timesheet.helper.app',
   appName: 'timesheet-helper',
   webDir: 'www',
   bundledWebRuntime: false
